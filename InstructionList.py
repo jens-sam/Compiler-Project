d800dd3a7c00c715201e454ef27dd5b883c55a75
jens-sam <114012084+jens-sam@users.noreply.github.com>
2026-05-13 11:07:45 -0600

    Update InstructionList.py

--- InstructionList.py
+++ InstructionList.py
@@ -5,15 +5,7 @@ from .instructions.La import La
 
 class InstructionList(UserList):
   def __str__(self):
-    #return "\n".join(self.data)
     return "\n".join([str(i) for i in self.data])
-    #return "\n".join([(str(a) if a is not None else "") for a in self.data])
-
-#  def __init__(self):
-#    self.data = list()
-
-#  def __str__(self):
-#    return str(self.data) #."\n".join(self.data)
 
   def getLast(self) -> Instruction:
     return self.data[-1]
