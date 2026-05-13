5395499553993a0b86452d9a37a1e8459d2f5880
jens-sam <114012084+jens-sam@users.noreply.github.com>
2026-05-13 11:07:26 -0600

    Update CodeObject.py

--- CodeObject.py
+++ CodeObject.py
@@ -17,14 +17,9 @@ class CodeObject:
         self.temp = None
         self.lval = None
 
-        # Add these attributes
-        self.cond_type = None  # For storing the condition type (e.g., "==", "<", etc.)
-        self.ltemp = None  # Temporary for the left operand
-        self.rtemp = None  # Temporary for the right operand
-
-        # # Debug
-        # print(
-        #     f"DEBUG: CodeObject initialized with STE={self.ste}, Type={self.type}, Temp={self.temp}, Lval={self.lval}")
+        self.cond_type = None  
+        self.ltemp = None  
+        self.rtemp = None 
 
     def __str__(self):
         sb = ";Current temp: " + (self.temp if self.temp is not None else "") + "\n" + ";IR Code: \n" + str(self.code)
@@ -34,13 +29,11 @@ class CodeObject:
         return self.code
 
     def getTemp(self):
-        # if self.temp is None:
-        #     print("DEBUG: getTemp called but temp is None!")
+
         return self.temp
 
     def isVar(self):
-        # result = self.ste is not None
-        # print(f"DEBUG: isVar={result}, STE={self.ste}")
+
         return self.ste is not None
 
     def getSTE(self):
